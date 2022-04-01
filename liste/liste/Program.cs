4b73906f636272c1ff14a5659ad2489a96a2f4d7
Obyssa <98898039+Obyssa@users.noreply.github.com>
2022-04-01 15:18:01 +0200

    Add files via upload

---
+++ liste/liste/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+using System.Collections.Generic;
+
+namespace liste
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //EX 1
+            //List<string> uneliste = new List<string>();
+            //uneliste.Add("wesh");
+            //uneliste.Add("bien");
+            //uneliste.Add("bien");
+            //uneliste.Add("ou");
+            //uneliste.Add("tu vas");
+            //foreach (string unElement in uneliste)
+            //{
+            //    Console.WriteLine(unElement);
+            //}
+
+            //EX 2
+            //Console.WriteLine("veuillez écrire un nombre");
+            //string nombreuser = Console.ReadLine();
+            //int nombreliste = int.Parse(nombreuser);
+            //List<int> uneliste = new List<int>();
+            //uneliste.Add(nombreliste);
+            //while (nombreliste != 0)
+            //{
+            //    Console.WriteLine("Veuillez entrée un autres chiffre");
+            //    nombreuser = Console.ReadLine();
+            //    nombreliste = int.Parse(nombreuser);
+            //    uneliste.Add(nombreliste);
+            //}
+
+            //foreach (int unElement in uneliste)
+            //{
+            //    Console.WriteLine(unElement);
+            //}
+
+            //EX 3
+            //List<int> uneliste = new List<int>();
+            //uneliste.Add(1);
+            //uneliste.Add(6);
+            //uneliste.Add(7);
+            //uneliste.Add(9);
+            //uneliste.Add(5);
+            //uneliste.Add(6);
+            //uneliste.Add(7);
+            //uneliste.Add(3);
+            //uneliste.Add(5);
+            //uneliste.Add(9);
+            //float adition= 0;
+            //float moyenne = 0;
+            //foreach (int unElement in uneliste)
+            //{
+            //    adition = adition + unElement;
+            //    
+            //    
+            //}
+            //moyenne = adition / 10;
+            //Console.WriteLine(moyenne);
+
+
+            //EX 4
+            List<int> uneliste = new List<int>();
+            uneliste.Add(1);
+            uneliste.Add(6);
+            uneliste.Add(7);
+            uneliste.Add(9);
+            uneliste.Add(5);
+            uneliste.Add(6);
+            uneliste.Add(7);
+            uneliste.Add(3);
+            uneliste.Add(5);
+            uneliste.Add(9);
+
+
+        }
+    }
+}
