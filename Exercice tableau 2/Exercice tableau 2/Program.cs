fc28e57c54e987bc72ef72269c72371491780354
Obyssa <98898039+Obyssa@users.noreply.github.com>
2022-04-01 15:19:05 +0200

    Add files via upload

---
+++ Exercice tableau 2/Exercice tableau 2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Exercice_tableau_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nombre = new int[4];
+            int test = 100;
+            Console.WriteLine("veuillez entrer 4 valeurs");
+            int a = 1;
+            for (int i = 0; i < nombre.Length; i++)
+            {
+                Console.WriteLine("valeur "+ a);
+                nombre[i] = int.Parse(Console.ReadLine());
+                a++;
+            }
+
+            for (int i = 0; i < nombre.Length; i++)
+            {
+                if (nombre[i] < test)
+                {
+                    test=nombre[i];
+                }
+                
+            }
+            Console.WriteLine(test);
+        }
+    }
+}
