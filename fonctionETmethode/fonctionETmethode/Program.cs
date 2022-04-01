ce9c068edef19f8f38d15f1b13311eddfc0e63c9
Obyssa <98898039+Obyssa@users.noreply.github.com>
2022-04-01 14:47:04 +0200

    Add files via upload

---
+++ fonctionETmethode/fonctionETmethode/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace fonctionETmethode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ex 1
+            int CalculSurfaceRectangle(int longueur, int largeur)
+            {
+
+             return  longueur * largeur;
+            }
+            Console.WriteLine("veuillez rentrée la longueur");
+            int l = int.Parse(Console.ReadLine());
+            Console.WriteLine("veuillez rentrée la largeur");
+            int L = int.Parse(Console.ReadLine());
+            int surface = CalculSurfaceRectangle(l, L);
+            Console.WriteLine(surface);
+
+            //ex 2
+            //int nbrmax = 1000;
+            //string phr = Console.ReadLine();
+            //affichée(nbrmax, phr);
+            //void affichée(int nbr, string ph)
+            //{
+            //for (int i=0; i < nbr; i++)
+            //{
+            //Console.WriteLine(ph);
+            //}
+            //}
+
+            int[] tab1 = new int[5];-
+            int longueurChaine(string chaine)
+            {
+
+            }
+
+        }
+    }
+}
