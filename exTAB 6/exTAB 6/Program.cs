83e0c016d427fd2a8f33d74d8c541e0727cf8bab
Obyssa <98898039+Obyssa@users.noreply.github.com>
2022-04-01 14:46:26 +0200

    Add files via upload

---
+++ exTAB 6/exTAB 6/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace exTAB_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] tab = new int[10];
+            for (int i = 0; i < tab.Length; i++)
+            {
+                Console.WriteLine("veuillez entrez la valeur " + i);
+                string string1 = Console.ReadLine();
+                int int1 = int.Parse(string1);
+                tab[i] = int1;
+            }
+            int moyenne = 0;
+            for(int i = 0; i < tab.Length; i++)
+            {
+                moyenne = moyenne + tab[i];
+            }
+            moyenne = moyenne / tab.Length;
+            for(int i = 0; i < tab.Length; i++)
+            {
+                if(moyenne < tab[i])
+                {
+                  Console.WriteLine("les valeurs en dessu de la moyenne sont");
+                  Console.WriteLine(tab[i]);
+                }
+            }
+        }
+    }
+    
+}
