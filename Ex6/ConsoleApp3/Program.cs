16de406a4a2257d6575f8d0d57eadf3f7ad13770
Obyssa <98898039+Obyssa@users.noreply.github.com>
2022-04-01 14:43:11 +0200

    Add files via upload

---
+++ Ex6/ConsoleApp3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ecri un nobre toutou");
+
+            string nombre = Console.ReadLine();
+
+            int resultat = int.Parse(nombre);
+
+            if (resultat == 0) {
+
+                Console.WriteLine("votre chiffre est pas ouf");
+            }
+            else if (resultat > 0)
+            {
+
+                Console.WriteLine("Votre chiffre est positif");
+            }
+            else if (resultat < -0)
+            {
+
+                Console.WriteLine("Votre chiffre est negatif");
+
+            }
+        }
+    }
+}
