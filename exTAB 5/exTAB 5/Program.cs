7326b4fb42d0b067efe0a2c832fbfeee8532cd17
Obyssa <98898039+Obyssa@users.noreply.github.com>
2022-04-01 14:45:55 +0200

    Add files via upload

---
+++ exTAB 5/exTAB 5/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace exTAB_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] tab = new int[100];
+            
+            for (int i = 0; i < tab.Length; i++)
+            {
+                Random aleatoire = new Random();
+                int entierUnChiffre = aleatoire.Next(10);
+                tab[i] = entierUnChiffre;
+            }
+            Console.WriteLine("saisisser la valeur max svp");
+            string string1 = Console.ReadLine();
+            int int1 = int.Parse(string1);
+            for (int i = 0; i< tab.Length; i++)
+            {
+                if (tab[i] > int1)
+                {
+                    tab[i] = 0;
+                }
+                Console.WriteLine(tab[i]);
+            }
+        }
+    }
+}
