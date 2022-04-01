816c8edc8b5998a28f6b8ad922c1cf30eac89113
Obyssa <98898039+Obyssa@users.noreply.github.com>
2022-04-01 15:19:46 +0200

    Add files via upload

---
+++ UtiliserDebug/UtiliserDebug/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace UtiliserDebug
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /*Rappel Debug : 
+            F11 mode Pas A Pas
+            F5 passer d'une point d'arrét à un autre ou poursuivre le programme
+            Shift + F11 passer à la fonction suivante
+
+            */
+            int johan, ghislain, solene, enzo, quentin, theo, alexy, boris, maxence, hugo, thibault, matheo, noe, louis, remi, mathias, abel, mylan, zoe, logan, esteban;
+
+
+            for (int i = 0; i < 1000; i++)
+            {
+                Console.WriteLine("");
+                for (int j = 0; j < 20; j++)
+                {
+                    Random random = new Random();
+                    int alea = random.Next(1, 200);
+                    Console.Write(alea);
+                }
+
+            }
+            johan = 4 * 456 * 78;
+            ghislain = 2 * 4 * 87;
+            solene = 7 * 8 * 6;
+            enzo = 2 * 3 * 4 * 43 * 45;
+            quentin = 2 * 116 * 44 * 4 * 45;
+            theo = 2 * 9 * 43 * 45 * 45;
+            alexy = 6 * 3 * 13 * 45 * 45;
+            boris = 2 * 8 * 4 * 45 * 45;
+            maxence = 11 * 3 * 4 * 45 * 45;
+            hugo = 2 * 3 * 44 * 45 * 45;
+            thibault = 33 * 3 * 4 * 45 * 45;
+            matheo = 62 * 38 * 4 * 45 * 45;
+             noe = 2 * 3 * 4 * 45 * 45;
+            louis = 2 * 14 * 45 * 45 * 45;
+            remi = 2 * 45 * 21 * 45 * 45;
+            mathias = 2 * 3 * 46 * 45 * 45;
+            abel = 6 * 43 * 3 * 45 * 45;
+            mylan = 2 * 3 * 36 * 45 * 45;
+            zoe = 2 * 3 * 4 * 45 * 45;
+            logan = 12 * 3 * 4 * 45 * 45;
+            esteban = 2 * 63 * 44 * 45 * 45;
+
+            //Donnez le contenu de votre variable sans ecrire une seule ligne de code ( utiliser le débug) résultat = 19083600
+
+            for (int i = 0; i < 1000; i++)
+            {
+                Console.WriteLine("");
+                for (int j = 0; j < 20; j++)
+                {
+                    Random random = new Random();
+                    int alea = random.Next(1, 200);
+                    Console.Write(alea);
+                }
+
+            }
+
+
+            int total = johan + ghislain + solene + enzo + quentin + theo + abel + alexy + boris + remi + mathias;
+            // donnez le contenu de la variable total MOINS la valeur de votre variable  Modulo 22
+            // exemple (total - maVariable % 22) = -6
+            
+
+            int variableFin = 0;
+        }
+    }
+}
---
+++ structure/structure/Program.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace structure
+{
+    class Program
+    {
+        //struct Librairie
+        //{
+            //public string Titre;
+            //public string Auteur;
+            //public double Prix;
+            ///constructeur de la structure
+            //public Librairie(string Titre, string Auteur, double Prix)
+            //{
+                //this.Titre = Titre;
+                //this.Auteur = Auteur;
+                //this.Prix = Prix;
+            //}
+        //}
+        //static void Main(string[] args)
+        //{
+            ///création des trois structures éléves
+            //Librairie livre1 = new Librairie("L'Art de la guerre", "Sun Tzu", 5.90 );
+            //Librairie livre2 = new Librairie("L'attaque des titant", "Hajime Isayama" , 5.60 );
+            //Librairie livre3 = new Librairie("No Game No Life", "Yū Kamiya", 7.99 );
+            //Librairie livre4 = new Librairie("Erase", "Kei Sanbe", 7.66 );
+            //Librairie livre5 = new Librairie("Bleach", "Tite Kubo", 6.90 );
+            ///création d'une liste
+            //List<Librairie> uneListe = new List<Librairie>();
+            ///modifie la classe de l'éléve Forest Gump
+            //unEleve3.classe = "103";
+            ///ajout des eleves à la liste
+            //uneListe.Add(livre1);
+            //uneListe.Add(livre2);
+            //uneListe.Add(livre3);
+            //uneListe.Add(livre4);
+            //uneListe.Add(livre5);
+            ///parcours de la liste
+            
+            //foreach (Librairie item in uneListe)
+            //{
+                
+                //Console.WriteLine(item.Titre + " " + item.Auteur + " " + item.Prix);
+            //}
+            //Console.Read();
+        //}
+
+        
+        struct Magasin
+        {
+
+            public int Référence;
+            public string Nom;
+            public int Prix;
+            public int Quantité;
+
+            public Magasin(int Référence, string Nom, int Prix, int Quantité)
+            {
+
+                this.Référence = Référence;
+                this.Nom = Nom;
+                this.Prix = Prix;
+                this.Quantité = Quantité;
+
+
+            }
+
+            static void Main(string[] args)
+            {
+                string val = "start";
+
+                List<Magasin> uneListe = new List<Magasin>();
+                
+                while(val == "start")
+                {
+                    Console.WriteLine("Saisir la Référence");
+                    int Référence = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Saisir le Nom");
+                    string Nom = Console.ReadLine();
+                    Console.WriteLine("Saisir le Prix");
+                    int Prix = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Saisir la Quantité");
+                    int Quantité = int.Parse(Console.ReadLine());
+
+                    Magasin Article = new Magasin(Référence, Nom, Prix, Quantité-1); 
+
+                    uneListe.Add(Article);
+
+                    Console.WriteLine("Veuillez écrire start pour continué ou stop pour arréter");
+
+                    val = Console.ReadLine();
+
+                }
+
+               
+
+                foreach(Magasin item in uneListe)
+                {
+
+                    Console.WriteLine(item.Référence + " " + item.Nom + " " + item.Prix + " " + item.Quantité);
+
+                    if (item.Quantité<4)
+                    {
+
+                        Console.WriteLine("Attention stock faible!!!!!!!!!!!!");
+
+
+                    }
+
+                }
+
+
+
+            }
+
+
+
+        }
+
+
+    } 
+}
+
